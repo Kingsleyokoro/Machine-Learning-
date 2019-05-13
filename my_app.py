3c2c806b11b6d7f2a7458885ebce64cb5de6d8a3
Kingsleyokoro <50120436+Kingsleyokoro@users.noreply.github.com>
2019-05-12 23:30:20 -0600

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,1 +1,18 @@
-print("Hello World")
+users = {
+"user1" : "password1",
+"user2" : "password2",
+"user3" : "password3"
+}
+
+def accept_login(users,user,password):
+   for key,values in users.items():
+       if key==values:
+           return True
+       else:
+           return False
+ 
+            
+if accept_login(users, "user1", "password1") :
+  print("login successful!")
+else :
+  print("login failed...")
